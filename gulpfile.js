130743cd610974818fb45ad5f34dac7a693dd11c
Rcong <654846387@qq.com>
2016-04-13 10:05:08 +0800

    添加 gulp-minify-css

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var less = require('gulp-less');
-var path = require('path');
 var minifyCSS = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 
@@ -14,11 +13,9 @@ gulp.task('compress', function () {
 });
 
 gulp.task('less', function () {
-  return gulp.src('./*.less')
+  return gulp.src('./waterfall.less')
     .pipe(autoprefixer())
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
+    .pipe(less())
     .pipe(minifyCSS())
     .pipe(gulp.dest('./lib'));
 });
